bc4da1d4b7c7b33a65e18d316bf6f579f8205fd9
Adentalis <daniel.n96@t-online.de>
2022-07-08 18:07:14 +0200

    solve

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var memoTimes = [238, 270, 188, 167, 181, 269, 228, 168, 324, 150];
-var solveTimes = [137, 95, 0, 170, 0, 0, 0, 137, 150, 0];
-var failTimes = [0, 0, 137, 0, 95, 159, 134, 0, 0, 100];
+var memoTimes = [238, 270, 188, 167, 181, 269, 228, 168, 324, 150, 190];
+var solveTimes = [137, 95, 0, 170, 0, 0, 0, 137, 150, 0, 0];
+var failTimes = [0, 0, 137, 0, 95, 159, 134, 0, 0, 100, 136];
 var labels = Array.from({ length: memoTimes.length }, (_, i) => i + 1);
 
 var ctx = document.getElementById("myChart").getContext("2d");
